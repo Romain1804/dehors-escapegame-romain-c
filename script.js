171c0ccb98bbec4cdc21010dde2ace6559a9162d
Romain1804 <romaincharles42@gmail.com>
2026-05-20 17:04:38 +0200

    dehors final

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-fetch("data.json")
+fetch("escape-game.json")
     .then(rep => {
         return rep.json()
     })
@@ -6,176 +6,238 @@ fetch("data.json")
         // a ce niveau on devrait avoir dans la console les infos renvoyé par l’API
 
         console.log(data)
+        afficheActivite(data.activites)
+        afficheavantage(data.avantagesClients)
+        affichetemoignage(data.temoignages)
+        afficheHerotitre(data.nomCommercial)
+        affichephraseaccroche(data.phraseAccroche)
+        afficheherobtn(data.texteAppelAction)
+
+
     })
 
-//role afficher dans le body les donné du tableau a leur emplacement 
-// parametre tableau de donné
+
+
+
+
+    //role afficher les cartes acrtivité dans la div activite container
+// parametre tableau des activités
 //return rien elle affiche
 
 
-    function affichedonne(tableaudonne){
+function afficheActivite(tableauActivite) {
 
-        tableaudonne.forEach(donnees => {
+    tableauActivite.forEach(Activite => {
+
+
+        document.querySelector("#activitecontainer").innerHTML += `
             
-            let user =`
+             <div class="card">
+
+                    <img src="${Activite.imageurl}" alt="" class="w100">
+
+                    <div class="padding16">
+                        <h2 class="padbottom20 bolder">${Activite.nom}</h2>
+                        <p class="padbottom20">${Activite.description}
+                        </p>
+                        <button" class="btn ">Réserver</button>
+                    </div>
+                </div>
             
-               <header class="flex spacebetween bglightblack aligncenter">
-        <img src="logo-escape-1.png" alt="" class="w70">
-        <nav>
-            <a href="" class="padright colorwhite ">activite</a>
-            <a href="" class="padright colorwhite">temoignage</a>
-            <a href="" class="padright colorwhite">avantage client</a>
-            <a href="" class="padright colorwhite">contact</a>
-        </nav>
-    </header>
+            `
 
-    <main class="police bgsable">
-        <!-- hero -->
-        <section class="bgimg1 colorwhite flex  aligncenter txtcenter">
+    });
 
-            <div>
-                <h1>nom commercial</h1>
-                <p class="padbottom20">Découvrez l'aventure ultime avec Dehors!, votre destination d'escape game à Paris
-                </p>
-                <button class="btn margbottom30">reserver votre aventure</button>
+    // faire la même fonction pour chaque section 
 
-            </div>
+}
 
 
-        </section>
-        <!-- activite -->
-        <section class="container padbottom50 margtop30">
 
-            <div>
-                <h2 class="center padbottom20 colorwhite">
-                    activite
-                </h2>
-            </div>
 
-            <div class="flex spacebetween">
 
-                <!-- premiere activite -->
 
-                <div class="card flex aligncenter  ">
+//role afficher les avantage clients dans la div avantagescontainer
+// parametre tableau avantages clients
+//return rien elle affiche
 
-                    <img src="logo-escape-1.png" alt="" class="w50">
+function afficheavantage(tableauavantage) {
+    tableauavantage.forEach(avantage => {
 
-                    <div class="w50">
-                        <h2>nom</h2>
-                        <p class="padbottom20">Explorez un manoir mystérieux rempli d'énigmes et de mystères.
-                            Parviendrez-vous à résoudre les énigmes et à vous échapper avant que le temps ne s'écoule ?
-                        </p>
-                        <button>reserver l'aventure</button>
-                    </div>
+        document.querySelector("#avantagecontainer").innerHTML += `
+            
+            <p class="padbottom10 margbottom30 center">${avantage}</p>
+
+            `
+
+    });
+}
+
+
+
+
+
+
+
+//temoignage
+
+//role afficher les temoignage client dans la div temoignagecontainer
+// parametre tableau temoignage client
+//return rien elle affiche
+
+function affichetemoignage(tableautemoignage) {
+
+
+    tableautemoignage.forEach(temoignage => {
+
+        document.querySelector("#temoignagecontainer").innerHTML += `
+    
+     <div class="card flex aligncenter colunm">
+                    <p class="padbottom20">${temoignage.prenom}</p>
+                    <p class="padbottom20">${temoignage.typeExperience}</p>
+                    <p class="padbottom20">${temoignage.commentaire}</p>
+                    <p class="margbottom30">${etoiles(temoignage.note)}</p>
                 </div>
+             
+    `
 
-                <!-- seconde activite -->
-
-                <div class="card flex aligncenter">
-
-                    <img src="logo-escape-1.png" alt="" class="w50">
-
-                    <div class="w50">
-                        <h2>nom</h2>
-                        <p class="padbottom20">Explorez un manoir mystérieux rempli d'énigmes et de mystères.
-                            Parviendrez-vous à résoudre les énigmes et à vous échapper avant que le temps ne s'écoule ?
-                        </p>
-                        <button>reserver l'aventure</button>
-                    </div>
-                </div>
+    });
 
 
-                <!-- troisieme activite -->
-
-                <div class="card flex aligncenter">
-
-                    <img src="logo-escape-1.png" alt="" class="w50">
-
-                    <div class="w50">
-                        <h2>nom</h2>
-                        <p class="padbottom20">Explorez un manoir mystérieux rempli d'énigmes et de mystères.
-                            Parviendrez-vous à résoudre les énigmes et à vous échapper avant que le temps ne s'écoule ?
-                        </p>
-                        <button>reserver l'aventure</button>
-                    </div>
-                </div>
-
-            </div>
-
-        </section>
-
-        <section class="bgimg2 flex aligncenter colunm txtcenter colorwhite">
-            <h2 class="padbottom10 margtop30">titre avantage client</h2>
-            <p class="padbottom10">avantage client</p>
-            <p class="padbottom10">avantage client</p>
-            <p class="margbottom30">avantage client</p>
-
-        </section>
-        <!-- temoignage -->
-        <section class="margtop30 padbottom50 container">
-            <h2 class="center padbottom20 colorwhite">temoignage</h2>
-            <div class="flex spacebetween">
-                <div class="card flex aligncenter colunm"><!-- 1er temoignage -->
-                    <p>prenom</p>
-                    <p>type d'experience</p>
-                    <p>commentaire</p>
-                    <p class="margbottom30">notes</p>
-                </div>
-
-                <div class="card flex aligncenter colunm"><!-- 2eme temoignage -->
-                    <p>prenom</p>
-                    <p>type d'experience</p>
-                    <p>commentaire</p>
-                    <p class="margbottom30">notes</p>
-                </div>
-
-                <div class="card flex aligncenter colunm"><!-- 3eme temoignage -->
-                    <p>prenom</p>
-                    <p>type d'experience</p>
-                    <p>commentaire</p>
-                    <p class="margbottom30">notes</p>
-                </div>
-            </div>
-        </section>
-
-    </main>
-
-
-    <footer class=" flex spacebetween container aligncenter margtop30 ">
+}
 
 
 
-        <img src="logo-escape-1.png" alt="" class="w70">
 
 
 
-        
-        <div class="">
-            <p class="padbottom10"><i class="ph ph-navigation-arrow"></i>16 rue bidule 42100 st Etienne</p>
 
 
 
-            <p><i class="ph ph-phone"></i>04 88 98 76 34</p>
-        </div>
+//role repeter les etoile 
+// parametre : nombre, le nombre d'etoiles a afficher
+//return : les etoies (chaine de caractere)
+//const repliq= <i class="ph ph-star"></i>
 
-        <div class="">
-            <p class="padbottom10"><i class="ph ph-envelope"></i>contact.dehors@orange.fr</p>
-
-
-
-            <p><i class="ph ph-clock"></i>8h30-12h30 14h-17h30</p>
-        </div>
+function etoiles(nombre) {
+    return "★".repeat(nombre) + "☆".repeat(5 - nombre)
+}
 
 
 
-    </footer>`
 
 
-            //document.querySelector("").innerHTML+=usercard
-
-        });
-
-// faire la même fonction pour chaque section 
 
 
-    }
+
+
+//hero
+//role afficher le hero dans la div hero
+// parametre : le nom de l'entreprise
+//return rien elle affiche
+
+function afficheHerotitre(nom) {
+
+    document.querySelector("#hero").innerHTML += `
+     <h1 class="padbottom20 bolder colorclair">${nom}</h1>
+              
+    `
+}
+
+
+
+
+
+
+
+
+
+//hero
+//role afficher le hero dans la div hero
+// parametre : la phrase d'accroche
+//return rien elle affiche
+
+function affichephraseaccroche(phrase) {
+
+    document.querySelector("#hero").innerHTML += `
+     <p class="padbottom20 bolder colorclair">${phrase}</p>
+              
+    `
+
+}
+
+
+
+
+
+
+
+
+
+//hero
+//role afficher le bouton dans la div hero
+// parametre : le bouton 
+//return rien elle affiche
+
+
+function afficheherobtn(bouton) {
+
+    document.querySelector("#hero").innerHTML += `
+
+<button class="btn margbottom30 btnsurvol">${bouton}</button>
+
+`
+}
+
+
+
+
+
+
+
+//footer survol 
+let survolcontact= document.getElementById("contact")
+
+survolcontact.addEventListener("mouseover",
+
+    function event (event){
+
+        event.target.style.color = "orange";
+
+        setTimeout(function()  {
+            event.target.style.color="black"
+        }, 300);
+    },false,
+)
+
+
+
+
+
+
+
+
+
+
+
+//header ( survol )
+
+let survolhead= document.getElementById("header")
+
+survolhead.addEventListener("mouseover",
+
+    function event (event){
+
+        event.target.style.color = "orange";
+
+        setTimeout(function()  {
+            event.target.style.color="black"
+        }, 300);
+    },false,
+)
+
+
+
+
+
+
